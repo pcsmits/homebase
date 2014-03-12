487388b78c80c25c1e8d5b846b05fbd2a26a4d59
Parker Smits <smits2010@gmail.com>
2014-03-12 14:47:30 -0500

    merging conflicts and adding new files

---
+++ root/homeBase/src/app/android/homeBase/testParker.java
@@ -0,0 +1,52 @@
+package app.android.homeBase;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import app.android.homeBase.GPSservice;
+
+public class testParker extends Activity {
+
+    Button btnShowLocation;
+
+    // GPSTracker class
+    GPSservice gps;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_parker);
+
+        btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSservice(testParker.this);
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
+    }
+
+}
