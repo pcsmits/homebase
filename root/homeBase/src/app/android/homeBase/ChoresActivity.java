740e1ef03a908742cab430155a4ff0432ec729da
Emanuel Rosu <rosu.emanuel@gmail.com>
2014-03-12 22:52:23 -0500

    work on chores + animations

--- root/homeBase/src/app/android/homeBase/ChoresActivity.java
+++ root/homeBase/src/app/android/homeBase/ChoresActivity.java
@@ -4,6 +4,9 @@ package app.android.homeBase;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Enumeration;
+
 import android.os.Bundle;
 import android.view.Display;
 import android.view.Gravity;
@@ -17,6 +20,16 @@ import com.beardedhen.androidbootstrap.BootstrapButton;
 
 public class ChoresActivity extends ActionBarActivity {
     ArrayList<BootstrapButton> choreContainers;
+    HashMap<BootstrapButton, ChoreInfo> choreDescriptions;
+
+    class ChoreInfo {
+        public String title;
+        public String information;
+        ChoreInfo(String title, String information) {
+            this.title = title;
+            this.information = information;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,39 +38,7 @@ public class ChoresActivity extends ActionBarActivity {
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.chores_choreContainer_button);
         choreContainers = new ArrayList<BootstrapButton>();
-
-        /*RelativeLayout container = new RelativeLayout(this);
-        RelativeLayout.LayoutParams rlp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        params.gravity = Gravity.CENTER;
-
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
-        int height = size.y;
-
-        params.height = (int)(height * 0.25f);
-        params.width = (int)(width * 0.50f);
-
-        rlp.height = 50;
-        rlp.width = 70;
-
-        BootstrapButton runtimeButton = new BootstrapButton(this);
-        //container.addView(runtimeButton);
-
-        String buttonText = "test";
-        runtimeButton.setText(buttonText);
-
-        runtimeButton.setRightIcon("fa-heart");
-
-        runtimeButton.setBootstrapType("success");
-
-        //layout.addView(container, rlp);
-        //layout.addView(runtimeButton, params);*/
+        choreDescriptions = new HashMap<BootstrapButton, ChoreInfo>();
 
         //this will eventually run through chores from parse and populate view accordingly, but this is a good framework
         //for creating bootstrap buttons programmaticaly from xml frameworks
@@ -70,8 +51,13 @@ public class ChoresActivity extends ActionBarActivity {
             layout.addView(myButton);
             String text = "Button " + i;
             myButton.setText(text);
-            myButton.setRightIcon("fa-heart");
             choreContainers.add(myButton);
+            String title = "Issue #: " + i;
+            String information = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et imperdiet diam, " +
+                    "dignissim tincidunt mauris. Donec euismod vehicula augue. Ut velit augue, commodo in malesuada in, " +
+                    "volutpat at turpis. Fusce vitae lectus metus. Etiam at dui nisi. Aenean gravida ligula sit amet adipiscing porta. " +
+                    "Fusce non accumsan dolor, sed iaculis quam. Mauris tincidunt nulla vitae eros semper pulvinar.";
+            choreDescriptions.put(myButton, new ChoreInfo(title, information));
         }
     }
 
@@ -80,7 +66,8 @@ public class ChoresActivity extends ActionBarActivity {
         BootstrapButton thisButton = (BootstrapButton) view.findViewById(R.id.login_testChores_button);
         thisButton.setText("Clicked");
         Intent intent = new Intent(ChoresActivity.this, ChoreInfoActivity.class);
-        intent.putExtra("info", "I'm putting this custom information here");
+        intent.putExtra("title", choreDescriptions.get(thisButton).title);
+        intent.putExtra("info", choreDescriptions.get(thisButton).information);
         startActivity(intent);
     }
 }
