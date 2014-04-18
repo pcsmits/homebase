b8add30920078694c4c2e765b82adf72fa9b5df7
Parker Smits <smits2010@gmail.com>
2014-04-17 19:14:58 -0500

    adding user class?

---
+++ root/homeBase/src/app/android/homeBase/HomeBaseUserStatus.java
@@ -0,0 +1,12 @@
+package app.android.homeBase;
+
+public class HomeBaseUserStatus {
+    public String username;
+    public Boolean isHome;
+
+    public HomeBaseUserStatus(String user, Boolean home)
+    {
+        username = user;
+        isHome = home;
+    }
+}
